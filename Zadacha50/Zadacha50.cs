c139417dd3392487d88fbb539857c41cde6e5aee
AlexIv <ivlieva.medphyslab.com>
2023-02-25 11:50:14 +0300

    Code to get an array added

--- Zadacha50/Zadacha50.cs
+++ Zadacha50/Zadacha50.cs
@@ -1,3 +1,38 @@
 ﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента 
 в двумерном массиве, и возвращает значение этого элемента 
 или же указание, что такого элемента нет.*/
+System.Console.WriteLine("Enter number of rows: "); 
+int M = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter number of columns: "); 
+int N = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter maximal number: "); 
+int MaxValue = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter minimal number: "); 
+int MinValue = Int32.Parse(System.Console.ReadLine());
+int [,] Matrix= Get2DArray(M,N,MaxValue,MinValue);
+Print2DArray (Matrix);
+
+int [,] Get2DArray (int M, int N, int MaxValue, int MinValue)
+{
+int[,] Array2D = new int [M,N];
+for (int i=0; i<Array2D.GetLength(0); i++)
+ {
+    for (int j=0; j<Array2D.GetLength(1); j++)
+    {
+      Array2D[i,j]= new Random().Next(MinValue, MaxValue+1);
+    }
+ }
+ return Array2D;
+}
+
+void Print2DArray (int [,] Matrix)
+{
+for (int i=0; i<Matrix.GetLength(0); i++)
+ {
+    for (int j=0; j<Matrix.GetLength(1); j++)
+    {
+      System.Console.Write($"{Matrix[i,j]} ");
+    }
+    System.Console.WriteLine();
+ }
+}
